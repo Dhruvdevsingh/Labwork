87974b852eca40dba2d65d4200815bf16328dbe5
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2023-11-22 06:55:18 +0530

    Add files via upload

---
+++ Program3a.c
@@ -0,0 +1,16 @@
+// Program to check if a number is even or odd
+#include<stdio.h>
+void main()
+{
+ int a;
+ printf("Enter the Number:");
+ scanf("%d",&a);
+ if(a%2==0)
+ {
+    printf("The number is Even");
+ }
+ else
+ {
+  printf("The number is odd");
+ }
+}
