cc683cf6704ceb870d17c3ce1dfc472265dfdd0f
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2023-11-24 21:33:42 +0530

    Add files via upload

---
+++ Program6c.c
@@ -0,0 +1,25 @@
+// program to search an element in an array using linear search
+#include<stdio.h>
+void main()
+{
+    int arr[30];
+    int num,ele,found=0,i;
+    printf("Enter the number of elements:");
+    scanf("%d",&num);
+    printf("Enter the elements:");
+    for(i=0;i<num;i++){
+    scanf("%d",&arr[i]);
+    }
+    printf("Enter the elements to search:");
+    scanf("%d",&ele);
+    for(i=0;i<num;i++){
+        if(arr[i]==ele){
+            printf("Element found at the index %d element:%d\n",i,ele);
+            found=1;
+            break;
+        }
+        }
+        if(!found){
+            printf("Element not found in the array");
+        }
+}
