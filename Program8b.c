5ecf50f952847be0567e9ca9e3138c75a379f38d
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2023-11-24 22:17:34 +0530

    Add files via upload

---
+++ Program8b.c
@@ -0,0 +1,40 @@
+// Program to check whether a given string is palindrome
+#include<stdio.h>
+#include<string.h>
+void printrev(char str[])
+{
+    int len=strlen(str);
+    printf("Given String:");
+    for(int i=len-1;i>=0;i--)
+    {
+        printf("%c",str[i]);
+    }
+    printf("\n");
+}
+int ispal(char str[])
+{
+    int len=strlen(str);
+    for(int i=0;i<len/2;i++)
+    {
+        if(str[i]!=str[len-i-1])
+        {
+           return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char input[100];
+    printf("Enter the String:");
+    scanf("%s",input);
+    printrev(input);
+    if(ispal(input))
+    {
+        printf("%s is a palindrome",input);
+    }
+    else
+    {
+        printf("%s is not a palindrome",input);
+    }
+}
