80b51c1b0d4577575a290f23a9d3fc1e7e7f8c7f
Dhruvdevsingh <147177787+Dhruvdevsingh@users.noreply.github.com>
2024-01-06 01:38:45 +0530

    Program11.c

--- Program11.c
+++ Program11.c
@@ -1,36 +1,87 @@
-//Title of program
-//Program to store information of a student using union
-#include<stdio.h>
-//function heading
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-//use of union
-union student
-{
-//variable declaration
-int rollno;
-char name[50];
-float avg;
-};
-//variable declaration
-int n,i;
-//declaration of union
-union student s;
-//output statement 
-printf("enter the student rollno\n");
-//input statement 
-scanf("%d",&s.rollno);
-//output statements
-printf("student rollno:%d\n",s.rollno);
-printf("enter the student name\n");
-//input statement 
-scanf("%s",s.name);
-//output statements
-printf("student name:%s\n",s.name);
-printf("enter the sudent average\n");
-//input statement 
-scanf("%f",&s.avg);
-//output statement
-printf("Average:%f\n",s.avg);
-return 0;
+    int *ptr;
+    int n, i;
+
+    // Using malloc() to allocate memory for 5 integers
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    ptr = (int *)malloc(n * sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Memory successfully allocated using malloc.\n");
+
+    // Reading and displaying elements using malloc
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("Elements entered using malloc:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    printf("\n");
+
+    // Using calloc() to allocate memory for 5 integers initialized to zero
+    ptr = (int *)calloc(n, sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Memory successfully allocated using calloc.\n");
+
+    // Reading and displaying elements using calloc
+    printf("Elements entered using calloc:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    printf("\n");
+
+    // Using realloc() to resize memory for 10 integers
+    int newSize = 2 * n;
+    ptr = (int *)realloc(ptr, newSize * sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("Memory reallocation failed.\n");
+        return 1;
+    }
+
+    printf("Memory successfully reallocated using realloc.\n");
+
+    // Reading and displaying elements after realloc
+    printf("Enter %d more elements:\n", n);
+    for (i = n; i < newSize; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("Elements entered after realloc:\n");
+    for (i = 0; i < newSize; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    printf("\n");
+
+    // Freeing allocated memory
+    free(ptr);
+    printf("Memory freed.\n");
+
+    return 0;
 }
